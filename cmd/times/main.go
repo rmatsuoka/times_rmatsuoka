d13b4d30a268c960c3bf3e8dac89bc3b487cc556
rmatsuoka <r.matsuoka@protonmail.com>
2024-12-30 12:56:43 +0900

    main

--- cmd/times/main.go
+++ cmd/times/main.go
@@ -1,22 +1,41 @@
-package times
+package main
 
 import (
+	"database/sql"
+	"log"
 	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/rmatsuoka/times_rmatsuoka/internal/api"
+	"github.com/rmatsuoka/times_rmatsuoka/internal/infra"
 	"github.com/rmatsuoka/times_rmatsuoka/internal/x/xhttp"
 	"github.com/rmatsuoka/times_rmatsuoka/internal/x/xslog"
+	"github.com/rmatsuoka/times_rmatsuoka/internal/x/xsql"
 )
 
 func main() {
-	logger := slog.New(xslog.NewContextHandler(slog.Default().Handler()))
+	logger := slog.New(xslog.NewContextHandler(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		AddSource: true,
+	})))
 	slog.SetDefault(logger)
 
+	infra.Init()
+	db, err := sql.Open("sqlite3", "file:local.db?_fk=1")
+	if err != nil {
+		panic(err)
+	}
+
 	mux := http.NewServeMux()
-	a := &api.API{}
+	a := &api.API{
+		DB: &xsql.SQLDB{DB: db},
+	}
 	a.Install(mux.Handle)
 
+	addr := ":8000"
+	log.Printf("listen on %s", addr)
 	handler := xhttp.LoggingHandler(mux)
-	http.ListenAndServe(":8080", handler)
+	if err := http.ListenAndServe(addr, handler); err != nil {
+		log.Fatal(err)
+	}
 }
